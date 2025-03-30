765627df4c44082a960bfd32d5d3801ae01112d0
Tyler Schloesser <tylerschloesser@gmail.com>
2025-03-30 15:40:47 -0700

    add pixi

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { Application } from 'pixi.js'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import invariant from 'tiny-invariant'
@@ -8,50 +9,27 @@ invariant(container)
 
 createRoot(container).render(
   <StrictMode>
-    <>TODO</>
+    <></>
   </StrictMode>,
 )
 
-const log = document.createElement('pre')
-document.body.appendChild(log)
-
-function appendLog(message: string) {
-  log.textContent = message + '\n' + log.textContent
-}
-
-let lastFrame = self.performance.now()
-const callback: FrameRequestCallback = () => {
-  const now = self.performance.now()
-  const dt = now - lastFrame
-  lastFrame = now
-  if (dt > 18) {
-    appendLog(`frame: ${dt.toFixed(2)}ms`)
-  }
-  self.requestAnimationFrame(callback)
-}
-self.requestAnimationFrame(callback)
-
-// document.addEventListener(
-//   'pointerdown',
-//   (ev) => {
-//     ev.preventDefault()
-//     appendLog(`pointerdown: ${ev.clientX}, ${ev.clientY}`)
-//   },
-//   { passive: false },
-// )
-
+// prevent swipe forward/backward on iOS
 document.addEventListener(
   'touchstart',
   (ev) => {
     ev.preventDefault()
-    if (ev.touches.length !== 1) {
-      return
-    }
-    const touch = ev.touches[0]
-    invariant(touch)
-    appendLog(
-      `touchstart: ${touch.clientX}, ${touch.clientY}`,
-    )
   },
   { passive: false },
 )
+
+const canvas = document.querySelector('canvas')
+invariant(canvas)
+
+const app = new Application()
+
+app.init({
+  antialias: true,
+  eventMode: 'none',
+  canvas,
+  resizeTo: window,
+})
